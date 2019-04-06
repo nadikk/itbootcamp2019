52194f070c6221f85c526b174201733e1a4b50f5
Nadia Krag <nadiakrag@Nadias-MacBook-Pro.local>
2019-04-07 01:48:40 +0200

    bootcamp

--- script.py
+++ script.py
@@ -76,7 +76,7 @@ def plot(data,preds,colors=["blue","black","red","cyan","magenta","green"]):
             Line2D([0],[0],color=colors[i]) for i in range(6)
         ]
     
-        ax.legend(custom_lines,classes,loc='center right')
+        ax.legend(custom_lines,classes,loc='center right',bbox_to_anchor=(1, 0.5))
         ax.set_title(col)
     plt.show()
 
